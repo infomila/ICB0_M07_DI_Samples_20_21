bf63161d6096c0c75598c3f190d19b51344f2955
bernat <bernat.orellana@gmail.com>
2021-10-04 21:25:31 +0200

    Formulari complet

--- UF1/20210930_Classes/DemoLlistes/MainPage.xaml.cs
+++ UF1/20210930_Classes/DemoLlistes/MainPage.xaml.cs
@@ -18,14 +18,46 @@ using Windows.UI.Xaml.Navigation;
 
 namespace DemoLlistes
 {
+
     /// <summary>
     /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        public enum Estat
+        {
+          VIEW,
+          MODIFICACIO,
+          ALTA
+        }
+
         // Atribut de la pàgina (tenen vida mentre la finestra està "viva")
         //List<Persona> persones = new List<Persona>();
         ObservableCollection<Persona> persones = new ObservableCollection<Persona>();
+        Estat estat = Estat.VIEW;
+
+
+        private void actualitzaInterficie()
+        {
+            btnBaixa.Visibility = (estat == Estat.VIEW) ? Visibility.Visible : Visibility.Collapsed;
+            btnNou.Visibility = btnBaixa.Visibility;
+            btnCancel.Visibility = estat!=Estat.VIEW ? Visibility.Visible : Visibility.Collapsed;
+            btnDesar.Visibility = btnCancel.Visibility;
+            lsbPersones.IsEnabled = (estat == Estat.VIEW);
+            btnBaixa.IsEnabled = lsbPersones.SelectedItem != null;
+        }
+
+        private void canviEstat(Estat estatNou)
+        {
+            estat = estatNou;
+            if (estat == Estat.VIEW)
+            {
+                MostraPersonaSeleccionada();
+            }
+            actualitzaInterficie();
+        }
+
 
         public MainPage()
         {
@@ -38,24 +70,125 @@ namespace DemoLlistes
             persones.Add(new Persona("11111112H", "Maria", DateTime.Now));
             persones.Add(new Persona("11111113H", "Joan", DateTime.Now));
             persones.Add(new Persona("11111114H", "Pep", DateTime.Now));
-
+ 
             lsbPersones.ItemsSource = persones;
             lsbPersones.DisplayMemberPath = "NomComplet";
-        }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Persona nova = new Persona(txbNIF.Text, txbNovaPersona.Text, dtpDataNaix.Date.Date);
-            persones.Add(nova);
-            //lsbPersones.ItemsSource = null;
-            //lsbPersones.ItemsSource = persones;
+            canviEstat(Estat.VIEW);
         }
+ 
 
         private void lsbPersones_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Persona p = (Persona) lsbPersones.SelectedItem;
-            //Persona p = persones[lsbPersones.SelectedIndex];
-            txbNovaPersona.Text = p.Nom;
+            //MostraPersonaSeleccionada();
+            canviEstat(Estat.VIEW);
+
+
+        }
+
+        private void MostraPersonaSeleccionada()
+        {
+            if (lsbPersones.SelectedItem != null)
+            {
+                Persona p = (Persona)lsbPersones.SelectedItem;
+                //Persona p = persones[lsbPersones.SelectedIndex];
+                txbNovaPersona.Text = p.Nom;
+                txbNIF.Text = p.NIF1;
+                dtpDataNaix.Date = p.DataNaixement;
+            }
+            else
+            {
+                txbNovaPersona.Text = "";
+                txbNIF.Text = "";
+                dtpDataNaix.Date = DateTime.Now;
+            }
+            //canviEstat(Estat.VIEW);
+        }
+
+        private void btnBaixa_Click(object sender, RoutedEventArgs e)
+        {
+            if (lsbPersones.SelectedItem != null)
+            {
+                persones.Remove((Persona)lsbPersones.SelectedItem);
+            }
+        }
+
+        private void btnNou_Click(object sender, RoutedEventArgs e)
+        {
+            canviEstat(Estat.ALTA);
+            txbNIF.Text = "";
+            txbNovaPersona.Text = "";
+            dtpDataNaix.Date = DateTime.Now;
+        }
+
+        private void btnDesar_Click(object sender, RoutedEventArgs e)
+        {
+            if (totsElsCampsSonValids())
+            {
+                if (estat == Estat.ALTA)
+                {
+                    Persona nova = new Persona(txbNIF.Text, txbNovaPersona.Text, dtpDataNaix.Date.Date);
+                    persones.Add(nova);
+                    lsbPersones.SelectedIndex = persones.Count - 1;
+                } else
+                {
+                    Persona paioSeleccionat = (Persona) lsbPersones.SelectedItem;
+                    paioSeleccionat.NIF1 = txbNIF.Text;
+                    paioSeleccionat.Nom = txbNovaPersona.Text;
+                    paioSeleccionat.DataNaixement = dtpDataNaix.Date.Date;
+                }
+            }
+            canviEstat(Estat.VIEW);
+        }
+
+        // TODO: Nyapa temporal que cal arreglar
+        private bool totsElsCampsSonValids()
+        {
+            return true;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            canviEstat(Estat.VIEW);            
+        }
+
+  
+        private void txbNIF_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            checkCanvis();
+        }
+ 
+
+        private void checkCanvis()
+        {
+            if (lsbPersones.SelectedItem != null)
+            {
+                Persona actual = (Persona)lsbPersones.SelectedItem;
+                Boolean HiHaCanvis = !(
+                                    actual.NIF1.Equals(txbNIF.Text) &&
+                                    actual.Nom.Equals(txbNovaPersona.Text) &&
+                                    actual.DataNaixement.Equals(dtpDataNaix.Date)
+                                        );
+                if (HiHaCanvis && estat == Estat.VIEW)
+                {
+                    canviEstat(Estat.MODIFICACIO);
+                }
+            }
+        }
+
+        private void txbNovaPersona_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            checkCanvis();
+        }
+
+        private void dtpDataNaix_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            checkCanvis();
+        }
+
+        private void dtpDataNaix_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            checkCanvis();
         }
     }
 }
--- UF1/20210930_Classes/DemoLlistes/Persona.cs
+++ UF1/20210930_Classes/DemoLlistes/Persona.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DemoLlistes
 {
-    class Persona
+    class Persona: INotifyPropertyChanged
     {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         private String NIF;
         private String nom;
         private DateTime dataNaixement;
@@ -19,15 +23,31 @@ namespace DemoLlistes
             DataNaixement = dataNaixement;
         }
 
-        public string NIF1 { get => NIF; set => NIF = value; }
-        public string Nom { get => nom; set => nom = value; }
-        public DateTime DataNaixement { get => dataNaixement; set => dataNaixement = value; }
+        public string NIF1 { get => NIF; set { 
+                NIF = value;
+                PropertyChanged?.Invoke(this,new PropertyChangedEventArgs("NIF1"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("NomComplet"));
+
+            }
+        }
+        public string Nom { get => nom; set { nom = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Nom"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("NomComplet"));
+            } }
+        public DateTime DataNaixement { get => dataNaixement; set
+            {
+                dataNaixement = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DataNaixement"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("NomComplet"));
+            }
+        }
 
         public string NomComplet { get { return Nom + " - " + NIF1 + " / " + DataNaixement; } }
 
+
         public override string ToString()
         {
-            return Nom;
+            return NomComplet;
         }
     }
 }
