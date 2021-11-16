5a91d0ff3f72b56959ef0cf1cbad4d065d6b9cf6
bernat <bernat.orellana@gmail.com>
2021-11-16 16:12:55 +0100

    INotifyPropertyChanged

---
+++ UF1/20211116_INotify/INotify/INotify/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using INotify.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace INotify
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void lsvPersones_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Persona actual = (Persona)lsvPersones.SelectedItem;
+            txtId.Text = actual.Id+"";
+            txtNom.Text = actual.Nom; 
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Persona actual = (Persona)lsvPersones.SelectedItem;
+            actual.Nom = txtNom.Text;
+            actual.Id = Int32.Parse( txtId.Text );
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            ObservableCollection<Persona> persones = new ObservableCollection<Persona>();
+            persones.Add(new Persona(1, "Paco1"));
+            persones.Add(new Persona(2, "Paco2"));
+            persones.Add(new Persona(3, "Paco3"));
+            persones.Add(new Persona(4, "Paco4"));
+            persones.Add(new Persona(5, "Paco5"));
+            persones.Add(new Persona(6, "Paco6"));
+            lsvPersones.ItemsSource = persones;
+        }
+    }
+}
---
+++ UF1/20211116_INotify/INotify/INotify/Model/Persona.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INotify.Model
+{
+
+    //https://github.com/Fody/PropertyChanged 
+    public class Persona: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private long id;
+        private String nom;
+
+        public long Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+
+        public Persona(long id, string nom)
+        {
+            Id = id;
+            Nom = nom;
+        }
+
+       /* public long Id
+        {
+            get => id;
+            set
+            {
+                if (value != id)
+                {
+                    id = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Id"));
+                }
+
+            }
+        }
+        public string Nom { get => nom;
+            set
+            {
+                if (value != nom)
+                {
+                    nom = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Id"));
+                }
+
+            }
+        }*/
+
+    }
+}
