bece7d8d523709d977ba8df43ebdf3a0f0a6585f
bernat <bernat.orellana@gmail.com>
2022-02-01 15:05:57 +0100

    a

--- UF2/20210201_DemoWIX/SetupProject/recopila_documentacio.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\heat.exe" dir ..\20210118_DemoDocumentacio\docs -dr DIR_DOC -cg CGR_DOCUMENTACIO -gg -g1 -sf -srd -var "var.PathDocumentacio" -out ./Documentacio.wxs
-
-pause
